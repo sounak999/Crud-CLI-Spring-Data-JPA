d325c094f5ee0075feac52822f37d3f2b5b878da
Sounak <sounaksaha333@gmail.com>
2025-04-06 22:47:58 +0530

    0.1 => project setup

---
+++ crudcli/src/main/java/com/command/crudcli/CrudcliApplication.java
@@ -0,0 +1,22 @@
+package com.command.crudcli;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CrudcliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudcliApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args) {
+		return runner -> {
+			System.out.println("Hello World!");
+		};
+	}
+	
+}
---
+++ crudcli/src/test/java/com/command/crudcli/CrudcliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.command.crudcli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudcliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
