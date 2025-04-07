9286eecb5fd5ae6ca62b7a66ea2ee0ffedcf874a
Sounak <sounaksaha333@gmail.com>
2025-04-07 22:31:38 +0530

    updating main app and added repository and transactional to DAO

--- src/main/java/com/command/crudcli/CrudcliApplication.java
+++ src/main/java/com/command/crudcli/CrudcliApplication.java
@@ -1,5 +1,7 @@
 package com.command.crudcli;
 
+import com.command.crudcli.dao.StudentDAO;
+import com.command.crudcli.entity.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,10 +15,22 @@ public class CrudcliApplication {
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner(String[] args) {
+	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
 		return runner -> {
-			System.out.println("Hello World!");
+			createStudent(studentDAO);
 		};
 	}
-	
+
+	private void createStudent(StudentDAO studentDAO) {
+		// create student object
+		System.out.println("Creating a new student object...");
+		Student student = new Student("Sounak", "Saha", "sounak.saha@google.com");
+
+		// save the student
+		studentDAO.save(student);
+
+		// display id of the saved student
+		System.out.println("Saved student. Generated id: " + student.getId());
+	}
+
 }
--- src/main/java/com/command/crudcli/dao/StudentDAOImpl.java
+++ src/main/java/com/command/crudcli/dao/StudentDAOImpl.java
@@ -3,7 +3,10 @@ package com.command.crudcli.dao;
 import com.command.crudcli.entity.Student;
 import jakarta.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+@Repository
 public class StudentDAOImpl implements StudentDAO {
 
     // define entity manager
@@ -17,6 +20,7 @@ public class StudentDAOImpl implements StudentDAO {
 
     // implement save method
     @Override
+    @Transactional
     public void save(Student student) {
         entityManager.persist(student);
     }
